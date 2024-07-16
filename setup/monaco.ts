b239c32802d955657e9d89c9c61c9159a91ebfa1
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2024-07-16 23:10:44 +0200

    Setup code editors

---
+++ setup/monaco.ts
@@ -0,0 +1,10 @@
+import { defineMonacoSetup, MonacoSetup } from "@slidev/types";
+
+export default defineMonacoSetup((() => {
+  return {
+    theme: {
+      dark: "vs-dark",
+      light: "vs",
+    },
+  };
+}) as MonacoSetup);
